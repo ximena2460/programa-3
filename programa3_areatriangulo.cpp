8cb34e0a076ec4c28a4fd6583ac7c74c548c9ea6
ximena2460 <ximenahuerta231@gmail.com>
2025-06-01 18:31:26 -0600

    Add files via upload

---
+++ programa3_areatriangulo.cpp
@@ -0,0 +1,14 @@
+ #include <conio.h>
+ #include <stdio.h>
+ main()
+ { float base, altura, area;
+ printf("calcula area de triangulo");
+ printf("\ndame la base: ");
+ scanf("%f", &base);
+ printf("\ndame la altura: ");
+ scanf("%f", & altura);
+ area=(base*altura)/2;
+ printf("la area es: %f", area);
+ getch();
+	
+ }
